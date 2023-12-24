e64736c96502d34fbc6e49f9756891429e5bac10
mithlesh devdas <mithleshdevdas580@gmail.com>
2023-12-24 11:34:29 +0530

    just lern

---
+++ varibles.js
@@ -0,0 +1,140 @@
+// javascript varibles
+
+const studentId = 564;
+// const not to recrad and not assin
+
+let studentEmail = 'mkdas@gmail.com';
+
+var password = '4551csncjks';
+
+console.table([studentId, studentEmail, password]);
+
+// varible naming in javascript
+
+let firstName = 'mithlesh';
+console.log(firstName);
+
+
+let last_name = 'devdas';
+console.log(last_name);
+
+
+let _my_age = 25;
+console.log(_my_age);
+
+let _contry = 'india';
+console.log(_contry);
+
+let $job = 'web devloper';
+console.log($job);
+
+let $my_num91 = 12345678910;
+console.log($my_num91);
+
+let pincode400203 = true;
+
+console.log(pincode400203);
+
+// varible in one line
+
+let id = 123, email = 'mkdas@gmail.com', pass = '56dsjd8*';
+console.log('id is:',id,'email:',email,'password:',pass);
+
+//  varible excecicese
+
+// # 💻 Day 1: Exercises
+
+// 1. Write a single line comment
+
+// [ans:] comment can make readble
+
+// 2. Write another single comment which says, _Welcome to Lerning OfJavaScript_
+
+// [ans:] _welcome to learnig of javacript
+
+// 3. write a multi line comment
+/*
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+Esse ullam vel cum, vero non repellendus libero est aperiam,
+ tempore tenetur, iusto delectus ad. Corrupti, voluptatibus. */
+ 
+// 4. Create a  variables and assign all primitive datatypes
+// string, boolean, undefined and null,number,synbol,BigInt data types
+
+/*
+[ans:]
+let myName = 'mithlesh'; 
+let myAge = 25;
+let iamMarried = false;
+let myJob = null;
+let mySalary;
+let myBirthMark = Symbol('@%');
+let myNum = 1313213131n;
+console.log( myName,myAge,iamMarried,myJob,mySalary,myBirthMark,myNum);
+
+
+    */
+
+// 5. create varible to check datatype of all varible
+/*
+[ans:]
+let study = 'college';
+console.log(study);
+console.log(typeof study);
+let mark = 350;
+console.log(mark);
+console.log(typeof mark);
+let studyCompelete = true;
+console.log(studyCompelete);
+console.log(typeof studyCompelete);
+let degree = null;
+console.log(degree);
+console.log(typeof degree);
+let course;
+console.log(course);
+console.log(typeof course);
+let collegeId = Symbol('@123');
+console.log(collegeId);
+console.log(typeof collegeId);
+let collegeCode = 511151555415n;
+console.log(collegeCode);
+console.log(typeof collegeCode);
+*/
+
+// 6. Declare four variables without assigning values
+/*
+let cars;
+let bike;
+let bycle;
+let toy;
+
+*/
+
+// 7. Declare four variables with assigned values
+/*
+let destopName = 'hp'
+let ram = '12GB';
+let prosser = 'intel 3rdgrn';
+let ssd = '120Gb';
+*/
+
+// 8. Declare variables to store your first name, last name, marital status, country and age in multiple lines
+/*
+let first_Name = 'mithlesh';
+let _last_name = 'devdas';
+let $materilSatus = false;
+let countryIn = 'india';
+let age25 = 25;
+*/
+
+// 9. Declare variables to store your first name, last name, marital status, country and age in single lines
+/*
+let fNmae = 'mk', lName = 'das', married = true, mycontry = 'india', _age = 30;
+*/
+
+
+
+
+
+
+    
