e24cfab3cee2acf8f16a2ebe147326184a48cf64
mithlesh devdas <mithleshdevdas580@gmail.com>
2023-12-24 11:48:53 +0530

    javacript correct naming type

---
+++ varibleNaming.js
@@ -0,0 +1,10 @@
+// javascript varible naming
+
+const $firstName = 'mithlesh';
+const _lastName = 'devdas';
+const $my_age = 25;
+const marriedOn25 = false;
+const job = null;
+let salary;
+const adhar__id = Symbol('@225');
+const num = 123456789n;
