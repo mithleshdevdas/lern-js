ecc490f5a49726fa9e5b45fa95da931e4cd6a90e
mithlesh devdas <mithleshdevdas580@gmail.com>
2023-12-24 19:28:59 +0530

    increment and discrement opertor to pre-increment and post increment

---
+++ opertors/increment.js
@@ -0,0 +1,41 @@
+// ### incremet opertor
+
+// 1. pre-increment
+let count = 0;
+console.log(count);
+console.log(++count);
+
+// 1. post-increment
+let count1 = 0;
+console.log(count1++);
+console.log(count1);
+
+// decrement opertor
+
+// 1. pre-decement
+
+let count2 = 2;
+console.log(count2);
+console.log(--count2);
+console.log(count2);
+
+// 2.post-decrement
+
+let count3 = 2;
+console.log(count3--);
+console.log(count3);
+
+// ternary opertors
+
+let isMarried = true;
+isMarried ? console.log('your wife name') : console.log('any girlfriend0');
+
+isMarried = false;
+isMarried?console.log('your wife name'):console.log('any girlfrind');
+
+
+let num = 10;
+num > 0 ? console.log(`${num} is grether than number`) : console.log(`${num} is less than number`);
+
+
+num > 11 ? console.log(`${num} is grether than number`) : console.log(`${num} is less than number`);
