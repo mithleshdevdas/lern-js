23454c8aa92c24adf790552936f58fd11eab3f5d
mithlesh devdas <mithleshdevdas580@gmail.com>
2023-12-24 12:28:14 +0530

    multi line varibles in javscript

---
+++ multilineVarible.js
@@ -0,0 +1,14 @@
+let first_Name = "mithlesh";
+console.log(first_Name);
+
+let _last_name = "devdas";
+console.log(_last_name);
+
+let $materilSatus = false;
+console.log($materilSatus);
+
+let countryIn = "india";
+console.log(countryIn);
+
+let age25 = 25;
+console.log(age25);
