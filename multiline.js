8a7203e1f3e69f38960a718ee148f331bcdbda62
mithlesh devdas <mithleshdevdas580@gmail.com>
2023-12-24 12:11:21 +0530

    javascript multiline comment

---
+++ multiline.js
@@ -0,0 +1,25 @@
+// javascript multiline comment
+
+/*
+let firstName = "mithlesh";
+console.log(firstName);
+
+let last_name = "devdas";
+console.log(last_name);
+
+let _my_age = 25;
+console.log(_my_age);
+
+let _contry = "india";
+console.log(_contry);
+
+let $job = "web devloper";
+console.log($job);
+
+let $my_num91 = 12345678910;
+console.log($my_num91);
+
+let pincode400203 = true;
+
+console.log(pincode400203);
+*/
