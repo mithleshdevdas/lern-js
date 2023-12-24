a8e274030e6b9e381cbcdbd47ca77236cc65555a
mithlesh devdas <mithleshdevdas580@gmail.com>
2023-12-24 19:29:29 +0530

    window methods like alrelt

---
+++ opertors/widow.js
@@ -0,0 +1,14 @@
+// arler
+
+alert('hii mithlesh');
+
+// promt
+let number = prompt('enter the number');
+console.log(number);
+alert(number);
+
+// window confirm method
+
+const agree = confirm('are you sure like to delete?');
+console.log(agree);
+
