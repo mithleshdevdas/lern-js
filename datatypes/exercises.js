92fd3dde40f30d55b0d121a726036a272e787033
mithlesh devdas <mithleshdevdas580@gmail.com>
2023-12-24 17:21:18 +0530

    pratical to number math object and string

---
+++ datatypes/exercises.js
@@ -0,0 +1,76 @@
+// 1. Declare a variable named challenge and assign it to an initial value **'30 Days Of JavaScript'**.
+
+let change = "lern the javascript"
+console.log(change);
+
+// print the string
+let string = 'maza ma';
+console.log(string);
+console.log(typeof string);
+
+// print the length of string
+console.log(change.length);
+
+// change to uppercase
+console.log(change.toUpperCase());
+
+// change the lowercase
+let lower = "HII I AM LEANIG IN JAVASCRIPT";
+console.log(lower.toLowerCase());
+
+// cut the first world using substr or substring method
+
+console.log(lower.substr(1));
+console.log(lower.substring(1));
+
+// slice out the "I AM LEANIG IN JAVASCRIPT"  from "HII I AM LEANIG IN JAVASCRIPT"
+
+console.log(lower.slice(3));
+
+// check if change content "lern" world of incudes method
+
+console.log(change.includes('lern'));
+
+// split the change string to array using split method
+
+console.log(change.split(' , '));
+
+// change "HII I AM LEANIG IN JAVASCRIPT" to "HII I AM LEANIG IN JAVA" using repalce method
+
+console.log(lower.replace('HII', 'WOW'));
+
+// WHAT is char hii in index use charat method
+console.log(lower.charAt(10));
+
+// what is charcode of j in lower
+
+console.log(lower.charCodeAt(0));
+
+// useindex of
+console.log(lower.indexOf('L'));
+
+// lastindexof
+console.log(lower.lastIndexOf('N'));
+
+// serch
+console.log(lower.search('IN'));
+
+// trim
+
+console.log("   mithlesh       ".trim());
+
+// statwith
+
+console.log("mithlesh".startsWith('m'));
+
+// endwith
+console.log("mithlesh".endsWith('m'));
+// match
+console.log("devdas".match('d'));
+
+// concat
+console.log("mithlesh".concat(' devdas'));
+
+// repet
+console.log(' mithlesh '.repeat(5));
+
