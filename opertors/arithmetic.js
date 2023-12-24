f946313ce6d342d9a890d6856985d544a4d93955
mithlesh devdas <mithleshdevdas580@gmail.com>
2023-12-24 19:25:50 +0530

    javascript aritimat opertor for add minus divid multypy and etc

---
+++ opertors/arithmetic.js
@@ -0,0 +1,22 @@
+// artimatic opertors
+let numOne = 10;
+let numTwo = 5;
+
+let addition = numOne + numTwo;
+console.log(addition);
+
+let subtraction = numOne - numTwo;
+console.log(subtraction);
+
+let mutiply = numOne * numTwo;
+console.log(mutiply);
+
+let divid = numOne / numTwo;
+console.log(divid);
+
+let remder = numOne % numTwo;
+console.log(remder);
+
+let power = numOne ** numTwo;
+console.log(power);
+
