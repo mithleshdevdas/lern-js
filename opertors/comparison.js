0ac1becaef4f13c23d85e31c4fb9e2d417da1021
mithlesh devdas <mithleshdevdas580@gmail.com>
2023-12-24 19:27:08 +0530

    comperiction opertor are like grether than less than qual not qual value and data types in javascript

---
+++ opertors/comparison.js
@@ -0,0 +1,49 @@
+// @ts-nocheck
+// comparision operators
+// grether than
+console.log(3 > 2);
+
+// grethen and qual
+console.log(3 >= 2);
+
+// less than
+console.log(2 < 3);
+
+// less than and qual
+console.log(2 <= 1);
+
+// not qual
+console.log(3 != 3);
+
+// doble qual 
+console.log(10 == '10');
+
+// triple qual
+console.log(10 === '10');
+
+// 0 and false is falsy value
+// isme value check 
+console.log(0 == false);
+// isme value or datatype check
+console.log(0 === false);
+
+// 0 and emty array is falsy value
+console.log(0 == '');
+console.log(0 === '');
+
+// positive  number is true value
+console.log(1 == true);
+console.log(1 === true);
+
+// undifined and null
+console.log(undefined == null);
+console.log(null === undefined);
+
+// number or nan not a number
+console.log(Number == NaN);
+console.log(NaN === NaN);
+
+// les string
+console.log('mithlesh'.length=='mithlesh'.length);
+console.log('mithlesh'.length=='devdas'.length);
+console.log('mithlesh'.length !=='mithlesh'.length);
