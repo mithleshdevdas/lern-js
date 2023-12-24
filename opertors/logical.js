ee146021e89e446a6e7f0bb8205ce3740bda6cb6
mithlesh devdas <mithleshdevdas580@gmail.com>
2023-12-24 19:28:06 +0530

    logical opertor is and && or || and !not qual

---
+++ opertors/logical.js
@@ -0,0 +1,55 @@
+// and opertor
+// &&
+// isme dono true hota hai tbi true hoga dono false hoga to false
+
+let check = 4 > 3 && 10 > 5;
+console.log(check);
+// true true = true
+
+let check1 = 4 > 3 && 10 < 5;
+console.log(check1);
+// true false = false
+
+let check2 = 4 > 3 && 10 < 5;
+console.log(check2);
+// false false = false
+
+
+
+
+
+// || or opertor
+
+// isme dono true true hota hai ek true or ek false bi true hota hai
+// dono false false hota hai
+
+const check3 = 4 > 3 || 10 > 5;
+console.log(check3);
+// true or true = true
+
+const check4 = 4 > 3 || 10 < 5;
+console.log(check4);
+// true or false = true
+
+const check5 = 4 < 3 || 10 < 5;
+console.log(check5);
+// false false = false
+
+// !
+const check6 = 4 > 3;
+console.log(check6);
+// true
+const check7 = !(4 > 3);
+console.log(check7);
+// false
+const lightOn = true;
+console.log(lightOn);
+// true
+const lightOff = !lightOn;
+console.log(lightOff);
+
+
+
+
+
+
