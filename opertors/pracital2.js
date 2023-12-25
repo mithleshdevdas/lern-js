ebf5c359ea4931b6a6c001e4f290a28c3250379e
mithlesh devdas <mithleshdevdas580@gmail.com>
2023-12-25 11:38:05 +0530

    promt ko achhe se ek bar padna padega

---
+++ opertors/pracital2.js
@@ -0,0 +1,25 @@
+// write a prome to promt your age
+// @ts-nocheck
+// let date = new Date();
+// let currentYear = date.getFullYear();
+// console.log(currentYear);
+// let birthYear = prompt('enter your bithyear');
+// console.log(birthYear);
+// let result = birthYear - currentYear;
+
+// console.log(confirm(`your age is ${result}`));
+
+
+let sideA = prompt('Enter side a');
+console.log(sideA);
+let sideB = prompt('Enter side B');
+console.log(sideB);
+let sideC = prompt('Enter side C');
+console.log(sideC);
+
+let res = sideA + sideB + sideC;
+console.log(alert(`The perimeter of the triangle is ${res} `));
+
+// promt sikhana padega
+
+
