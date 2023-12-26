08b7442270b73c348323c81470a821f9736ae31a
mithlesh devdas <mithleshdevdas580@gmail.com>
2023-12-26 20:37:20 +0530

    while loop kyu use krte hai pata krna hai

---
+++ loops/whileloop.js
@@ -0,0 +1,15 @@
+// increment while loop
+let i = 0;
+while (i <= 5) {
+    console.log(i);
+    i++;
+}
+
+// deincrement while loop
+
+
+let t = 1;
+while (t < 10) {
+    console.log(t * 2);
+    t++;
+}
