624690b9b9f01c1b2d41307f1d3ef59b7cef243a
mithlesh devdas <mithleshdevdas580@gmail.com>
2023-12-26 20:34:33 +0530

    excerices me dyan dena jaruuri hai

---
+++ function.js/partical.js
@@ -0,0 +1,19 @@
+const fullName = () => {
+   console.log('mithlesh devdas');
+}
+fullName()
+
+const fulname = (fname ='mithlesh', lname ='devdaas') => {
+    let full_name =fname + lname
+  return full_name
+    
+}
+console.log(fulname());
+
+// add number
+const addnum = (numone, numtwo) => {
+    let sum = numone + numtwo
+    return sum
+}
+console.log(addnum(5,20));
+
