41f1de02aef015cdd54f88938ae1f050356e176e
mithlesh devdas <mithleshdevdas580@gmail.com>
2023-12-26 20:35:34 +0530

    breck and continus loop ke part hai

---
+++ loops/break and continus.js
@@ -0,0 +1,17 @@
+// break
+
+for (let i = 0; i <= 5; i++){
+    if (i == 3) {
+        break
+    }
+    console.log(i);
+}
+
+// continue
+
+for (let i = 0; i <= 5; i++){
+    if (i == 2) {
+        continue
+    }
+    console.log(i);
+}
